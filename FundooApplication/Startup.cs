814d9abed35e299ed00b836e28c8c3f9e27cf2e2
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-10-13 17:39:15 +0530

    [Riya] Add : Web API

--- FundooApplication/Startup.cs
+++ FundooApplication/Startup.cs
@@ -1,11 +1,17 @@
+using FundooManager.IManager;
+using FundooManager.Manager;
 using FundooRepository.Context;
+using FundooRepository.IRepository;
+using FundooRepository.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace FundooApplication
 {
@@ -24,6 +30,17 @@ namespace FundooApplication
             services.AddControllers();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContextPool<UserDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("UserDbConnection")));
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserManager, UserManager>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Fundoo App",
+                    Version = "v1",
+                    Description = "Fundoo Application"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +59,14 @@ namespace FundooApplication
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Employee}/{action=AddEmployee}");
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fundoo App");
             });
         }
     }
--- FundooRepository/Repository/UserRepository.cs
+++ FundooRepository/Repository/UserRepository.cs
@@ -6,12 +6,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utility;
 
 namespace FundooRepository.Repository
 {
     public class UserRepository : IUserRepository
     {
         public readonly UserDbContext context;
+        Nlog nlog = new Nlog();
         public UserRepository(UserDbContext context)
         {
             this.context = context;
@@ -22,6 +24,7 @@ namespace FundooRepository.Repository
             register.Password = password;
             this.context.Register.Add(register);
             var result = this.context.SaveChangesAsync();
+            nlog.LogInfo("User Registered");
             return result;
         }
         public Register LoginUser(Login login)
@@ -32,12 +35,15 @@ namespace FundooRepository.Repository
                 var decryptPassword = DecryptPassword(result.Password);
                 if (decryptPassword.Equals(login.Password))
                 {
+                    nlog.LogInfo("User Logged In");
                     return result;
                 }
+                nlog.LogError("User not Logged In");
                 return null;
             }
             catch (Exception)
             {
+                nlog.LogError("Error in User Login");
                 return null;
             }
         }
@@ -52,10 +58,13 @@ namespace FundooRepository.Repository
                     input.Password = password;
                     this.context.Register.Update(input);
                     var result = this.context.SaveChangesAsync();
+                    nlog.LogInfo("Password Reset Successful");
                     return input;
                 }
+                nlog.LogError("Password Reset Unsuccessful");
                 return null;
             }
+            nlog.LogError("Error in Password Reset");
             return null;
         }
         //Password Encrypt and Decrypt
@@ -65,6 +74,7 @@ namespace FundooRepository.Repository
             byte[] encode = new byte[password.Length];
             encode = Encoding.UTF8.GetBytes(password);
             strmsg = Convert.ToBase64String(encode);
+            nlog.LogInfo("Password Encrypted");
             return strmsg;
         }
         public string DecryptPassword(string encryptpwd)
@@ -77,6 +87,7 @@ namespace FundooRepository.Repository
             char[] decoded_char = new char[charCount];
             Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
             decryptpwd = new string(decoded_char);
+            nlog.LogInfo("Password Decrypted");
             return decryptpwd;
         }
     }
