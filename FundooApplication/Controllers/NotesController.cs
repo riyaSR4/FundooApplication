3fc5d2751300b3f7d7ab19f1c0737a4541ab7949
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-10-16 13:29:16 +0530

    [Riya] Add : Web API

---
+++ FundooApplication/Controllers/NotesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        public readonly 
+    }
+}
